793e3bb0add4869f9d2bf88783eeaf06fed1ab55
cozycodding <cozycodding@gmail.com>
2021-08-14 13:43:01 +0700

    all

--- src/modules/Content.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Link, Switch, Route, BrowserRouter } from "react-router-dom";
-
-import Routes from "./Routes";
-export default function Content() {
-  return <Routes></Routes>;
-}
